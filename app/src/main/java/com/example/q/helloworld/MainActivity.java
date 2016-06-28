3fcfe55cc1a0438eeddae25bb182a591c445a143
wwiiiii <wwiiiii@naver.com>
2016-06-28 19:56:31 +0900

    asdf

--- app/src/main/java/com/example/q/helloworld/MainActivity.java
+++ app/src/main/java/com/example/q/helloworld/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity {
-    int asdf = 1234;
     public String[] contacts={"Lee Seungwoo",
                                 "Jung Taeyoung",
                                 "Gimun"};
