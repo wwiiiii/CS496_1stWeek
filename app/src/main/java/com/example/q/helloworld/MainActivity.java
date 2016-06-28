da5522ec054b26e4718633f84ea5a92e45cf52db
seungwoo <ksalsw1996@gmail.com>
2016-06-28 21:49:04 +0900

    ImageAdapter Added GridView Added res added

--- app/src/main/java/com/example/q/helloworld/MainActivity.java
+++ app/src/main/java/com/example/q/helloworld/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.q.helloworld;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
@@ -11,6 +16,9 @@ import android.widget.CheckBox;
 import android.widget.Button;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.GridView;
+import android.content.Context;
+import com.example.q.helloworld.ImageAdapter;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -35,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         tabHost.addTab(spec1);
 
         TabSpec spec2= tabHost.newTabSpec("Tab B").setContent(R.id.linearLayout2).setIndicator("Tab BB");
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
         tabHost.addTab(spec2);
 
         TabSpec spec3= tabHost.newTabSpec("Tab C").setContent(R.id.linearLayout3).setIndicator("Tab CC");
