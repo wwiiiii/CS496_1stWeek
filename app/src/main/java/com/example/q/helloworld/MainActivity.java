5debdf66f46bbdf2e78c1789eee755ae18a57849
wwiiiii <wwiiiii@naver.com>
2016-06-30 12:34:08 +0900

    json data 내부 저장소에 저장 및 수정 구현 (naive하게 원래 내용을 모두 읽은 뒤 새 요소를 추가하고 덮어쓰기)

--- app/src/main/java/com/example/q/helloworld/MainActivity.java
+++ app/src/main/java/com/example/q/helloworld/MainActivity.java
@@ -31,6 +31,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -53,18 +56,19 @@ public class MainActivity extends AppCompatActivity {
 
     // contacts JSONArray
     JSONArray contacts = null;
-
+    String jsonPath;
 
     ArrayList<Person> contactList;
     ArrayAdapter Adapter;
     Vibrator vibrator;
+    static final int BUFFER_SIZE = 8192;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
+        jsonPath = getApplicationContext().getFilesDir().getAbsolutePath() + "/Roche";
         TabHost tabHost=(TabHost) findViewById(R.id.tabHost);
         tabHost.setup();
 
@@ -106,11 +110,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    protected String loadJsonDataFromInternal(String dirPath)
+    {
+        String res = "";
+        byte[] b = new byte[BUFFER_SIZE];
+        try {
+            FileInputStream input = new FileInputStream(dirPath + "/mydata.json");
+            input.read(b);
+            res = new String(b).trim();
+            input.close();
+        }catch(Exception e){}
+        return res;
+    }
 
-    protected String loadJsonData(){
+    protected String loadJsonDataFromRes()
+    {
         InputStream is = getResources().openRawResource(R.raw.mydata);
         Writer writer = new StringWriter();
-        char[] buffer = new char[1024];
+        char[] buffer = new char[BUFFER_SIZE];
         try {
             Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
             int n;
@@ -122,9 +139,98 @@ public class MainActivity extends AppCompatActivity {
             try{is.close();}
             catch(Exception e){}
         }
-
-        return writer.toString();
+        return writer.toString().trim();
     }
+    public void debug(String str)
+    {
+        Log.v("mydebug",str);
+    }
+
+    public static boolean deleteDirectory(File path) {
+        if(!path.exists()) {
+            return false;
+        }
+
+        File[] files = path.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                deleteDirectory(file);
+            } else {
+                file.delete();
+            }
+        }
+
+        return path.delete();
+    }
+
+    protected boolean saveToIntDir(String jsonData, String dirPath)//성공시 true
+    {
+        File dir = new File(dirPath);
+        if(!dir.exists()) {
+            debug("saveToIntDir : Roche dir not exists");
+            boolean res = dir.mkdirs();
+            if (res == false) {
+                debug("saveToIntDir failed : dir generation rejected");
+                return false;
+            }
+            else debug("saveToIntDir : Roche dir created");
+        }
+        try {
+            PrintWriter pw = new PrintWriter(dirPath + "/mydata.json");
+            pw.println(jsonData);
+            pw.close();
+        }catch(Exception e){
+            debug("saveToIntDir failed : file write error : " + e.toString());
+            return false;
+        }
+        debug("saveToIntDir succeed : " + jsonData);
+        return true;
+    }
+
+    protected String loadJsonData(){
+        String res="";
+        File dir = new File(jsonPath);
+       // debug(""+deleteDirectory(dir));
+        if(!dir.exists())
+        {
+            debug("call loadJsonDataFromRes");
+            res = loadJsonDataFromRes();
+            saveToIntDir(res, dir.getAbsolutePath());
+            //그 후 디렉토리 생성하고 파일 복사해서 저장
+        }
+        else
+        {
+            debug("call loadJsonDataFromInternal");
+            res = loadJsonDataFromInternal(dir.getAbsolutePath());
+        }
+        debug("result length : " + res.length());
+        debug("result : " + res);
+        return res;
+    }
+
+    protected void savePersonToInternal(Person newbie)
+    {
+        try {
+            JSONObject jsonObj = new JSONObject(loadJsonData());
+
+            // Getting JSON Array node
+            JSONArray arr = jsonObj.getJSONArray(TAG_CONTACTS);
+            JSONObject jsonPerson = new JSONObject();
+            JSONObject jsonPhone = new JSONObject();
+            jsonPerson.put("name",newbie.name);
+            jsonPerson.put("email",newbie.email);
+            jsonPhone.put("mobile","01012344321");
+            jsonPhone.put("home","01012344321");
+            jsonPerson.put("phone",jsonPhone);
+            arr.put(jsonPerson);
+            saveToIntDir(jsonObj.toString(), jsonPath);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
     protected ArrayList<Person> jsonStringToList(String jsonStr)
     {
@@ -140,26 +246,29 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < contacts.length(); i++) {
                     JSONObject c = contacts.getJSONObject(i);
 
-                    String id = c.getString(TAG_ID);
+                    //String id = c.getString(TAG_ID);
                     String name = c.getString(TAG_NAME);
                     String email = c.getString(TAG_EMAIL);
-                    String address = c.getString(TAG_ADDRESS);
-                    String gender = c.getString(TAG_GENDER);
+                    //String address = c.getString(TAG_ADDRESS);
+                    //String gender = c.getString(TAG_GENDER);
 
-                    // Phone node is JSON Object
-                    JSONObject phone = c.getJSONObject(TAG_PHONE);
-                    String mobile = phone.getString(TAG_PHONE_MOBILE);
-                    String home = phone.getString(TAG_PHONE_HOME);
-                    String office = phone.getString(TAG_PHONE_OFFICE);
-
+                    // Phone node is JSON ObjectString mobile
+                    String mobile = "";
+                    if(c.has("phone") && !c.isNull("phone")) {
+                        JSONObject phone = c.getJSONObject(TAG_PHONE);
+                        mobile = phone.getString(TAG_PHONE_MOBILE);
+                        //String home = phone.getString(TAG_PHONE_HOME);
+                        //String office = phone.getString(TAG_PHONE_OFFICE);
+                    }
+                    else mobile = c.getString("mobile");
 
                     Person contact = new Person(name,email,mobile);
 
                     // adding contact to contact list
                     res.add(contact);
                 }
-            } catch (JSONException e) {
-                e.printStackTrace();
+            } catch (Exception e) {
+                debug("jsonStr to List error : " + e.toString());
             }
         }
         res.add(new Person("","",""));
@@ -167,9 +276,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public void buttonPushed(View view)
     {
-        /*TextView tv = (TextView)findViewById(R.id.name);
-        String str = tv.toString();
-        Log.w("tag",str);*/
         vibrator.vibrate(1000);
         GridLayout parent = (GridLayout)view.getParent();
         TextView mobile = (TextView)parent.getChildAt(2);
@@ -178,7 +284,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+num));
         startActivity(intent);
     }
-    int dummyn = 0;
+
     protected void addListContact()
     {
         Intent intent = new Intent(MainActivity.this, addActivity.class);
@@ -197,7 +303,9 @@ public class MainActivity extends AppCompatActivity {
                 String name = data.getStringExtra("data_name");
                 String email = data.getStringExtra("data_email");
                 String mobile = data.getStringExtra("data_mobile");
-                contactList.add(contactList.size()-1,new Person(name, email, mobile));
+                Person newb = new Person(name, email, mobile);
+                contactList.add(contactList.size()-1,newb);
+                savePersonToInternal(newb);
                 Adapter.notifyDataSetChanged();
             }
         }
